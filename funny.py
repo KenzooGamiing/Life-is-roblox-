96e9f2855eee948777d2f12b656c254f61f8410b
KenzooGamiing <154379039+KenzooGamiing@users.noreply.github.com>
2024-06-13 20:11:34 +0200

    Update funny.py

--- funny.py
+++ funny.py
@@ -5,3 +5,10 @@ time.sleep(1)
 print("welcome")
 time.sleep(1)
 print("Life is roblox")
+time.sleep(1)
+roblox = input("is life roblox ?").strip().lower()
+time.sleep(1)
+if roblox == 'yes':
+    print("yay")
+elif roblox == 'no':
+    print("me sad")
